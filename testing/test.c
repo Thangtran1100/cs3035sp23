ef13c3df53518806265a0a63f773e5aa5bf8cb94
Thangtran1100 <thangtrn117@gmail.com>
2023-03-01 21:37:49 -0800

    update

--- testing/test.c
+++ testing/test.c
@@ -9,22 +9,6 @@ typedef struct
     double hyp;
 } PythagoreanTriple;
 
-// PythagoreanTriple* create_triangle()
-// {
-//     struct PythagoreanTriple* triangle = (PythagoreanTriple*) malloc(3 * sizeof(PythagoreanTriple));
-
-//     int arrayIndex = 0;
-
-//     for(int indexRow = 0; indexRow <= 3; indexRow++)
-//     {
-//         for(int indexCol = 0; indexCol <= 3; indexCol++)
-//         {
-//             triangle[arrayIndex]
-//         }
-//     }
-// }
-
-
 void getTwoSides(PythagoreanTriple *triangle, double sideAIn, double sideBIn)
 {
     triangle->sideA = sideAIn;
@@ -41,21 +25,17 @@ void getSideAndHyp(PythagoreanTriple *triangle, double sideAIn, double hypIn)
     triangle->sideB = sqrt(pow(hypIn, 2) - pow(sideAIn, 2));
 }
 
-void print_pythagorean_triples(PythagoreanTriple *triangle)
-{
-    for (int i = 0; i < 3; i++)
-    {
-        printf("(%f, %f, %f)\n", triangle[i].sideA, triangle[i].sideB, triangle[i].hyp);
-    }
-}
-
 void main()
 {
-    struct PythagoreanTriple *triangle = (PythagoreanTriple *)malloc(3 * sizeof(PythagoreanTriple));
+    PythagoreanTriple *triangle = malloc(3* sizeof(PythagoreanTriple));
 
-    getTwoSides(triangle, 3, 4);
+    getTwoSides(&triangle[0], 3, 4);
+    getTwoSides(&triangle[1], 6, 8);
+    getSideAndHyp(&triangle[2], 1, 2);
 
-    print_pythagorean_triples(triangle);
+    printf("PythTriple [sideA = %f, sideB = %f, hyp = %f]\n", triangle[0].sideA, triangle[0].sideB, triangle[0].hyp);
+    printf("PythTriple [sideA = %f, sideB = %f, hyp = %f]\n", triangle[1].sideA, triangle[1].sideB, triangle[1].hyp);
+    printf("PythTriple [sideA = %f, sideB = %f, hyp = %f]\n", triangle[2].sideA, triangle[2].sideB, triangle[2].hyp);
 
     free(triangle);
 }
